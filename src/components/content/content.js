1e041ae30c5af5a07283c7df855edf55e516ce54
Jacopo Giola <jacopo.giola@itstalentfactory.onmicrosoft.com>
2021-10-29 12:23:15 +0200

    Wireframe–Step 4

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -1,11 +1,13 @@
 import './content.css';
 import Totrepos from '../totrepos/totrepos';
 import Nav from '../nav/nav';
+import Table from '../table/table';
 
 const Content = () => {
     return (
         <div className='content'>
             <Totrepos />
+            <Table />
             <Nav />
         </div>
     )
---
+++ src/components/table/table.js
@@ -0,0 +1,15 @@
+import './table.css';
+
+const Table = () => {
+    return (
+        <div className='tableCont'>
+            <table>
+                <tr><th>Cognome</th><th>Nome</th><th>Utente githb</th><th>Nome repo</th><th>Totale commit</th><th>Data creazione</th><th>Ultimo aggiornamento</th></tr>
+                <tr><td>Giola</td><td>Jacopo</td><td>JacopoGiola99</td><td>Giola-Jacopo-github</td><td>4</td><td>29/10/2021</td><td>29/10/2021</td></tr>
+                <tr><td>...</td><td>...</td><td>...</td><td>...</td><td>...</td><td>...</td><td>...</td></tr>
+            </table>
+        </div>
+    )
+}
+
+export default Table;
