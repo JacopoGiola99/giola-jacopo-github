b975171942e356439c5ee03eb5b4c61a5fac1b20
Jacopo Giola <jacopo.giola@itstalentfactory.onmicrosoft.com>
2021-10-29 12:52:02 +0200

    Wireframe–Step 5

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -2,11 +2,13 @@ import './content.css';
 import Totrepos from '../totrepos/totrepos';
 import Nav from '../nav/nav';
 import Table from '../table/table';
+import Search from '../search/search';
 
 const Content = () => {
     return (
         <div className='content'>
             <Totrepos />
+            <Search />
             <Table />
             <Nav />
         </div>
---
+++ src/components/search/search.js
@@ -0,0 +1,12 @@
+import './search.css';
+
+const Search = () => {
+    return (
+        <div className='search'>
+            <button>Search</button>
+            <input type='text' placeholder='write here'></input>
+        </div>
+    )
+}
+
+export default Search;
