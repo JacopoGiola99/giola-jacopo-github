8545920e9874a14dab819abef3aebbb490c999b7
Jacopo Giola <jacopo.giola@itstalentfactory.onmicrosoft.com>
2021-11-01 17:20:52 +0100

    Version 0.0.3

---
+++ src/Api/Repos.js
@@ -0,0 +1,8 @@
+import * as data from './Mock/Esempio.json';
+
+const listRepos = () => {
+    let repos = data;
+    return repos;
+}
+
+export default listRepos;
--- src/components/table/table.js
+++ src/components/table/table.js
@@ -9,16 +9,17 @@ const Table = () => {
                     <th>Nome</th>
                     <th>Utente githb</th>
                     <th>Nome repo</th>
-                    <th>Totale commit</th
-                    ><th>Data creazione</th
-                    ><th>Ultimo aggiornamento</th>
+                    <th>Totale commit</th>
+                    <th>Data creazione</th>
+                    <th>Ultimo aggiornamento</th>
                 </tr>
                 <tr>
                     <td>Giola</td>
                     <td>Jacopo</td>
                     <td>JacopoGiola99</td>
                     <td>Giola-Jacopo-github</td>
-                    <td>4</td><td>29/10/2021</td>
+                    <td>4</td>
+                    <td>29/10/2021</td>
                     <td>29/10/2021</td>
                 </tr>
                 <tr>
