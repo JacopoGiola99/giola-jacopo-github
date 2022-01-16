f98e85faae4696f3b76ce0b65a1a689e9dacd904
Jacopo Giola <jacopo.giola@itstalentfactory.onmicrosoft.com>
2022-01-16 16:16:25 +0100

    v 1.0.2 formattazione date e aggiornamento Row

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,51 +1,101 @@
-import './Row.css';
-import { useState } from 'react';
+//import './Row.css';
 import { DateTime } from "luxon";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import Table from '@mui/material/Table';
+import TableCell from '@mui/material/TableCell';
+import TableRow from '@mui/material/TableRow';
+import TableBody from '@mui/material/TableBody';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
-const Row = (props) => {
-    const [isOpen, setIsOpen] = useState(props.arrayDettagli);
+// Data formato italiano  
+const formatDateCreation = (date) => {
+    const result = DateTime.fromISO(date).setLocale('it').toFormat('dd LLLL yyyy');
+    return result;
+}   
+const formatDateUpdate = (date) => {
+    const result = DateTime.fromISO(date).setLocale('it').toFormat('dd LLLL yyyy HH:mm');
+    return result;
+}
 
-    const handleClick = () => {
-        setIsOpen(!isOpen);
-        //props.setArrayDettagli(!props.ArrayDettagli);
-    }
-
-    // Data formato italiano  
-    function formatDateCreation (date){
-        const result = DateTime.fromISO(date).setLocale('it').toFormat('dd LLLL yyyy');
-        return result;
-    }   
-    function formatDateUpdate (date){
-        const result = DateTime.fromISO(date).setLocale('it').toFormat('dd LLLL yyyy HH:mm');
-        return result;
-    }
+const Row = (props, key) => {
+    const { row } = props;
+    const totCommit = props.commit;
+    const [open, setOpen] = React.useState(false);
+    const id = 'repos' + props.row.id;
+    console.log(id);
 
     return (
-        <tbody>
-            <tr className="row">
-                <td>{props.repos.surname}</td>
-                <td>{props.repos.name}</td>
-                <td>{props.repos.login}</td>
-                <td>{props.repos.repoName}</td>
-                <td>{props.commit}</td>
-                <td>{formatDateCreation(props.repos.creationDate)}</td>
-                <td>{formatDateUpdate(props.repos.lastUpdate)}</td>
-                <td><button onClick={() => handleClick()}>{isOpen ? 'Nascondi' : 'Mostra'} dettagli</button></td>
-            </tr>
-            {isOpen &&
-                <tr className="information">
-                    <td>{props.repos.surname}</td>
-                    <td>{props.repos.name}</td>
-                    <td>{props.repos.login}</td>
-                    <td>{props.repos.repoName}</td>
-                    <td>{props.commit}</td>
-                    <td>{formatDateCreation(props.repos.creationDate)}</td>
-                    <td>{formatDateUpdate(props.repos.lastUpdate)}</td>
-                    <td></td>
-                </tr>
-            }
-        </tbody>
-    )
+        <React.Fragment>
+            <TableRow key={id} sx={{ '& > *': { borderBottom: 'unset', background: 'rgb(0, 151, 121)' } }}>
+                <TableCell align="left">{row.cognome}</TableCell>
+                <TableCell align="left">{row.nome}</TableCell>
+                <TableCell align="left">{row.utenteGithub}</TableCell>
+                <TableCell align="left">{row.nomeRepo}</TableCell>
+                <TableCell align="left">{totCommit}</TableCell>
+                <TableCell align="left">{formatDateCreation(row.dataCreazione)}</TableCell>
+                <TableCell align="left">{formatDateUpdate(row.ultimoAggiornamento)}</TableCell>
+                <TableCell>
+                    <IconButton
+                        aria-label="expand row"
+                        size="small"
+                        onClick={() => setOpen(!open)}
+                    >
+                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell style={{ paddingBottom: 0, paddingTop: 0, background: 'rgb(222, 142, 73)' }} colSpan={8} >
+                    <Collapse in={open} timeout="auto" unmountOnExit >
+                        <Table aria-label="dettagli" >
+                            <TableBody>
+                                {row.dettagli.map((dettagliRow) => (
+                                    <TableRow key={dettagliRow.date}>
+                                        <TableCell align="left">{dettagliRow.cognome}</TableCell>
+                                        <TableCell align="left">{dettagliRow.nome}</TableCell>
+                                        <TableCell align="left">{dettagliRow.utenteGithub}</TableCell>
+                                        <TableCell align="left">{dettagliRow.nomeRepo}</TableCell>
+                                        <TableCell align="left">{totCommit}</TableCell>
+                                        <TableCell align="left">{formatDateCreation(dettagliRow.dataCreazione)}</TableCell>
+                                        <TableCell align="left">{formatDateUpdate(dettagliRow.ultimoAggiornamento)}</TableCell>
+                                        <TableCell align="left"></TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
+    );
 }
+
+Row.propTypes = {
+    row: PropTypes.shape({
+        cognome: PropTypes.string.isRequired,
+        nome: PropTypes.string.isRequired,
+        utenteGithub: PropTypes.string.isRequired,
+        nomeRepo: PropTypes.string.isRequired,
+        totaleCommit: PropTypes.number.isRequired,
+        dataCreazione: PropTypes.string.isRequired,
+        ultimoAggiornamento: PropTypes.string.isRequired,
+        id: PropTypes.number.isRequired,
+        dattagli: PropTypes.arrayOf(
+            PropTypes.shape({
+                cognome: PropTypes.string.isRequired,
+                nome: PropTypes.string.isRequired,
+                utenteGithub: PropTypes.string.isRequired,
+                nomeRepo: PropTypes.string.isRequired,
+                totaleCommit: PropTypes.number.isRequired,
+                dataCreazione: PropTypes.string.isRequired,
+                ultimoAggiornamento: PropTypes.string.isRequired,
+            }),
+        ),
+    }).isRequired,
+};
 
 export default Row;
--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from 'react';
+import Row from '../Row/Row';
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -10,8 +8,6 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 const createData = (cognome, nome, utenteGithub, nomeRepo, totaleCommit, dataCreazione, ultimoAggiornamento, id) => {
     return {
@@ -36,83 +32,6 @@ const createData = (cognome, nome, utenteGithub, nomeRepo, totaleCommit, dataCre
         ],
     };
 }
-
-const Row = (props, key) => {
-    const { row } = props;
-    const totCommit = props.commit;
-    const [open, setOpen] = React.useState(false);
-    const id = 'repos' + props.row.id;
-    console.log(id);
-
-    return (
-        <React.Fragment>
-            <TableRow key={id} sx={{ '& > *': { borderBottom: 'unset', background: 'rgb(0, 151, 121)' } }}>
-                <TableCell align="left">{row.cognome}</TableCell>
-                <TableCell align="left">{row.nome}</TableCell>
-                <TableCell align="left">{row.utenteGithub}</TableCell>
-                <TableCell align="left">{row.nomeRepo}</TableCell>
-                <TableCell align="left">{totCommit}</TableCell>
-                <TableCell align="left">{row.dataCreazione}</TableCell>
-                <TableCell align="left">{row.ultimoAggiornamento}</TableCell>
-                <TableCell>
-                    <IconButton
-                        aria-label="expand row"
-                        size="small"
-                        onClick={() => setOpen(!open)}
-                    >
-                        {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-                    </IconButton>
-                </TableCell>
-            </TableRow>
-            <TableRow>
-                <TableCell style={{ paddingBottom: 0, paddingTop: 0, background: 'rgb(222, 142, 73)' }} colSpan={8} >
-                    <Collapse in={open} timeout="auto" unmountOnExit >
-                        <Table aria-label="dettagli" >
-                            <TableBody>
-                                {row.dettagli.map((dettagliRow) => (
-                                    <TableRow key={dettagliRow.date}>
-                                        <TableCell align="left">{dettagliRow.cognome}</TableCell>
-                                        <TableCell align="left">{dettagliRow.nome}</TableCell>
-                                        <TableCell align="left">{dettagliRow.utenteGithub}</TableCell>
-                                        <TableCell align="left">{dettagliRow.nomeRepo}</TableCell>
-                                        <TableCell align="left">{totCommit}</TableCell>
-                                        <TableCell align="left">{dettagliRow.dataCreazione}</TableCell>
-                                        <TableCell align="left">{dettagliRow.ultimoAggiornamento}</TableCell>
-                                        <TableCell align="left"></TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </Collapse>
-                </TableCell>
-            </TableRow>
-        </React.Fragment>
-    );
-}
-
-Row.propTypes = {
-    row: PropTypes.shape({
-        cognome: PropTypes.string.isRequired,
-        nome: PropTypes.string.isRequired,
-        utenteGithub: PropTypes.string.isRequired,
-        nomeRepo: PropTypes.string.isRequired,
-        totaleCommit: PropTypes.number.isRequired,
-        dataCreazione: PropTypes.string.isRequired,
-        ultimoAggiornamento: PropTypes.string.isRequired,
-        id: PropTypes.number.isRequired,
-        dattagli: PropTypes.arrayOf(
-            PropTypes.shape({
-                cognome: PropTypes.string.isRequired,
-                nome: PropTypes.string.isRequired,
-                utenteGithub: PropTypes.string.isRequired,
-                nomeRepo: PropTypes.string.isRequired,
-                totaleCommit: PropTypes.number.isRequired,
-                dataCreazione: PropTypes.string.isRequired,
-                ultimoAggiornamento: PropTypes.string.isRequired,
-            }),
-        ),
-    }).isRequired,
-};
 
 const BigTable = (props) => {
 
